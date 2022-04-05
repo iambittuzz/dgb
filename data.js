5f519478e97ac6638fd75965119f43f3eb28602f
iambittuzz <89698917+iambittuzz@users.noreply.github.com>
2022-04-05 15:30:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://Visnu0p:1751@bittu.gv1tl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5265424629:AAH05mayiS3Q2dSXdvTosXGt7eBxGWhxU7g',//bot token
+    
+    bot_name:'COINBASEdemobot',//bot username without @
+    
+    bot_admin:1211389957,// admin telegram id
+    
+    channelsList : ['@DeadlyAirdrops'],// channels list
+    
+    reffer_bonus:0.003,// refferal bonus amount
+    
+    min_wd:0.1,//min withdrawal amount
+    
+    daily_bonus:0.001,// daily bonus amount
+    
+    currency:'', //bot cuurency
+    
+    cb_api_key:'',//coinbase api key
+    
+    cb_api_secret:'',// coinbase secret key
+    
+    cb_account_id:'',// counbase account id for the bot currency
+    
+    payment_channel:'@boompayoutz'// payment channel username
+    
+    }
